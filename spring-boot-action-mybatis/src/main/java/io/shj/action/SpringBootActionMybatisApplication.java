b800e5d816ef371ee2a2f58a69e3bc9466156457
lishijiee <2424782003@qq.com>
2023-05-23 20:40:24 +0800

    mybatis整合测试

---
+++ spring-boot-action-mybatis/src/main/java/io/shj/action/SpringBootActionMybatisApplication.java
@@ -0,0 +1,13 @@
+package io.shj.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActionMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActionMybatisApplication.class, args);
+    }
+
+}
---
+++ spring-boot-action-mybatis/src/test/java/io/shj/action/SpringBootActionMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shj.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActionMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
